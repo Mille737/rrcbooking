50744dfe47bea46e515a06b15d4a51223751a05a
nicspiegelhauer <Nicolaj.spiegel@gmail.com>
2019-12-03 19:50:05 +0100

    CRUD2

---
+++ src/test/java/com/example/rrcbooking/RrcbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rrcbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrcbookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
